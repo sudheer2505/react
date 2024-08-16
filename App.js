6c111422d719b3ee23f83b3b1444eeec71fcf548
sudheer2505 <sudheer93.b@gmail.com>
2024-08-16 15:07:36 +0530

    react episode-01 changes

---
+++ App.js
@@ -0,0 +1,9 @@
+const header = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", { id: "heading" }, "I am h1 Tag"),
+        React.createElement("h2", { id: "heading" }, "I am h2 Tag")
+        ]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(header);
