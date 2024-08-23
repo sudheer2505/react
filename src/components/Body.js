ae09c18c47ff9f9fa1f60cca01c84c57d216b5ef
sudheer2505 <sudheer93.b@gmail.com>
2024-08-23 08:00:12 +0530

    React Hook useState implemented in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  updateFilterdData = () => {
+    let filterdData = listOfRestaurants.filter((res) => res.rating > 4.7);
+    console.log("filter = ", filterdData);
+    setListOfRestaurants(filterdData);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <button className="filter-btn" onClick={updateFilterdData}>Filter Data</button>
       <div className="cards-container">
-        {resList.map((res) => (
+        {listOfRestaurants.map((res) => (
           <RestaurantCard key={res.id} resData={res} />
         ))}
       </div>
